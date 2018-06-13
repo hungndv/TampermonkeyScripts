103ecbd3396d6acae6fd6cea665d09d9cfaa3be2
hungndvaswig <hung.duc.vinh.nguyen@steadfasttech.com.au>
2018-06-13 10:35:08 +0700

    Edit

--- fxstreetAutoClick.user.js
+++ fxstreetAutoClick.user.js
@@ -6,26 +6,67 @@
 // @author       You
 // @match        https://www.fxstreet.com/news/latest?q=&hPP=15&idx=FxsIndexPro&p=0
 // @grant        none
+// @run-at       document-idle
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
-    //document.querySelector(".fxs_bgHeader").remove();
-    //document.querySelector("#fxs_home > div:nth-child(1)").remove();
-    //document.querySelector("div[id^='brokersspreads_'] > section").remove();
 
-    var timerVar = setInterval (function() {DoMeEverySecond (); }, 3000);
+    // Your code here...
+    console.log("Script is attached.");
+    // Select the node that will be observed for mutations
+    var targetNode = document.querySelector("body");
+    var nodeToObserve;
 
-    function DoMeEverySecond ()
-    {
-        //--- Your code here.
-        console.clear();
-        console.log("Script is running...");
-        var elementToBeClicked = document.querySelector("div[id^='fxs_search_alert_advancedsearchmanager_']");
-        if (elementToBeClicked !== undefined)
-        {
-            elementToBeClicked.click();
+    // Options for the observer (which mutations to observe)
+    var config = { attributes: true, childList: true, subtree: true };
+
+    // Callback function to execute when mutations are observed
+    var callback = function (mutationsList) {
+        for (var mutation of mutationsList) {
+            if (mutation.type == 'childList') {
+                nodeToObserve = document.querySelector("div[id^='fxs_search_alert_advancedsearchmanager_']");
+                if (nodeToObserve) {
+                    console.log('Observe the node by selector "div[id^=\'fxs_search_alert_advancedsearchmanager_\']"');
+                    observeNode(nodeToObserve);
+                    observer.disconnect();
+                }
+            }
+        }
+    };
+
+    var callback2 = function (mutationsList) {
+        for (var mutation of mutationsList) {
+            if (mutation.type == 'attributes') {
+                if (mutation.attributeName === 'class') {
+                    console.log('The ' + mutation.attributeName + ' attribute was modified.');
+                    if (nodeToObserve.getAttribute('class').indexOf('fxs_hideElements') === -1) {
+                        nodeToObserve.click();
+                    }
+
+                    //document.querySelector(".fxs_bgHeader").style.display = "none";
+                    //document.querySelector("#fxs_home > div:nth-child(1)").style.display = "none";
+                    //document.querySelector("div[id^='brokersspreads_'] > section").style.display = "none";
+                }
+            }
+        }
+    };
+
+    // Create an observer instance linked to the callback function
+    var observer = new MutationObserver(callback);
+
+    // Start observing the target node for configured mutations
+    observer.observe(targetNode, config);
+
+    // Later, you can stop observing
+    //observer.disconnect();
+
+    var observer2;
+    function observeNode(nodeToObserve) {
+        if (!observer2) {
+            console.log('Create the observer2');
+            observer2 = new MutationObserver(callback2);
+            observer2.observe(nodeToObserve, config);
         }
     }
-    // Your code here...
 })();
